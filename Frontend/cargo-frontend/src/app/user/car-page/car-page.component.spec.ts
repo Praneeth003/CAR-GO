a215f5582185f5bc87804269488e3fd8ef79eca7
Rahul Edara <rahuledara@Rahuls-MBP.lan>
2022-10-12 17:40:34 -0400

    FrontEnd:: added car-go component

---
+++ Frontend/cargo-frontend/src/app/user/car-page/car-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarPageComponent } from './car-page.component';
+
+describe('CarPageComponent', () => {
+  let component: CarPageComponent;
+  let fixture: ComponentFixture<CarPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CarPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Frontend/cargo-frontend/src/app/user/car-page/car-page.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SharedService } from 'src/app/shared/shared.service';
+
+@Component({
+  selector: 'app-car-page',
+  templateUrl: './car-page.component.html',
+  styleUrls: ['./car-page.component.css']
+})
+export class CarPageComponent implements OnInit {
+
+  id = ""
+  carData:any = {}
+  disp= true;
+  errorDisp = false;
+  constructor(private http: HttpClient, private httpService: SharedService,  private route: ActivatedRoute, private router:Router) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('carId');
+    if(this.id )
+    if(this.id == null && this.id == undefined || this.id == ""){
+      this.router.navigate(["/user/journey-details"]); 
+    }
+    this.getCardData()
+    // if(this.carData == null && this.carData == undefined){
+    //   this.router.navigate(["/user/journey-details"]); 
+    // }
+  }
+
+  async getCardData(){
+    this.disp = false;
+    this.errorDisp = false;
+    let endpoint = "variant_by_id/" + this.id
+    let result = await this.httpService.get(endpoint).toPromise();
+    if(result != null && result != undefined){
+      let sList =result['variantList'];
+      if (sList && sList.length > 0) {
+        this.carData = sList[0];
+      }else{
+        this.errorDisp = true;
+      }
+    }else{
+      this.errorDisp = true;
+    }
+  }
+}
